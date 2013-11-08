d83228f187d910588823bab3deda50ee26975876
litzi0815 <danny@litzinetz.de>
2013-11-08 14:56:55 +0100

    Corrected typing mistake

--- installer/index.php
+++ installer/index.php
@@ -7,7 +7,7 @@ if($_GET['installer']=='')
 	echo 'Welcome to OpenVoucher installer.<br><br>
 	Before you proceed, make sure that you have at least two interfaces configured on your machine. The internal interface, where the guests are connected to, <b>must</b> have a static IPv4 address. 
 	<br><br>
-	I is <b>strongly recommended</b> that you don\'t connect the internal and external interfaces to the same switch, unless they are in different VLANs.<br><br>
+	It is <b>strongly recommended</b> that you don\'t connect the internal and external interfaces to the same switch, unless they are in different VLANs.<br><br>
 	To proceed, select the operating system of the server:<br><br>
 	<form action="'.$_SERVER['PHP_SELF'].'" method="get">
 	<select name="installer" size="1" class="formstyle">
