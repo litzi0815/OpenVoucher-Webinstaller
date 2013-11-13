ef9f6165397996b37a3b6f88fdfd0c0a915ce218
litzi0815 <danny@litzinetz.de>
2013-11-13 16:27:24 +0100

    Began to implement update-function to installer

--- installer/installer_debian.php
+++ installer/installer_debian.php
@@ -7,7 +7,7 @@ class InstallerDebian
 	
 	function __construct()
 	{
-		define('INSTALLERVERSION','0.100');
+		define('INSTALLERVERSION','0.2.0');
 		session_start();
 		$this->g = new gui(INSTALLERVERSION);
 		if(trim($_GET['step'])=='' || (!is_numeric($_GET['step']) && $_GET['step']!='reset'))
@@ -15,6 +15,13 @@ class InstallerDebian
 			$this->step=1;
 		} else {
 			$this->step=$_GET['step'];
+		}
+		
+		if($_GET['type']=='update')
+		{
+			$_SESSION['update']=true;
+		} else {
+			$_SESSION['update']=false;
 		}
 		
 		if($this->step==1) $this->FormSysdata();
@@ -96,19 +103,25 @@ class InstallerDebian
 	private function FormSysdata()
 	{
 		$this->g->Header();
-		echo '<form action="'.$_SERVER['PHP_SELF'].'?step=2" method="post">
-		On which user is apache running? <input type="text" name="apacheuser" class="formstyle" size="20" value="www-data"><br>
-		Path to iptables: <input type="text" name="iptables" class="formstyle" size="20" value="/sbin/iptables"><br>
-		Path to arp: <input type="text" name="arp" class="formstyle" size="20" value="/usr/sbin/arp"><br>
-		Path to temp dir: <input type="text" name="tempdir" class="formstyle" size="20" value="/var/tmp/"><br>
-		Path to www-root: <input type="text" name="wwwroot" class="formstyle" size="20" value="/var/www/"><br><br>
-		Which interface are the guests connected to? <input type="text" name="if-internal" class="formstyle" size="20" value="eth0"><br>
-		Which interface is connected to the internet? <input type="text" name="if-external" class="formstyle" size="20" value="eth1"><br>
-		What\'s your internal IP (guest interface)? <input type="text" name="ip-internal" class="formstyle" size="20" value="10.0.0.1"><br>
-		Which authentication type do you want to use? <input type="radio" name="auth" value="mac-only" class="formstyle" checked> MAC only 
-		<input type="radio" name="auth" value="mac-ipv4" class="formstyle"> MAC and IPv4 fallback 
-		<input type="radio" name="auth" value="ipv4-only" class="formstyle"> IPv4 only
-		<br><br>
+		echo '<form action="'.$_SERVER['PHP_SELF'].'?step=2" method="post">';
+		if(!$_SESSION['update'])
+		{
+			echo 'On which user is apache running? <input type="text" name="apacheuser" class="formstyle" size="20" value="www-data"><br>
+			Path to iptables: <input type="text" name="iptables" class="formstyle" size="20" value="/sbin/iptables"><br>
+			Path to arp: <input type="text" name="arp" class="formstyle" size="20" value="/usr/sbin/arp"><br>';
+		}
+		echo 'Path to temp dir: <input type="text" name="tempdir" class="formstyle" size="20" value="/var/tmp/"><br>
+		Path to www-root: <input type="text" name="wwwroot" class="formstyle" size="20" value="/var/www/"><br><br>';
+		if(!$_SESSION['update'])
+		{
+			echo 'Which interface are the guests connected to? <input type="text" name="if-internal" class="formstyle" size="20" value="eth0"><br>
+			Which interface is connected to the internet? <input type="text" name="if-external" class="formstyle" size="20" value="eth1"><br>
+			What\'s your internal IP (guest interface)? <input type="text" name="ip-internal" class="formstyle" size="20" value="10.0.0.1"><br>
+			Which authentication type do you want to use? <input type="radio" name="auth" value="mac-only" class="formstyle" checked> MAC only 
+			<input type="radio" name="auth" value="mac-ipv4" class="formstyle"> MAC and IPv4 fallback 
+			<input type="radio" name="auth" value="ipv4-only" class="formstyle"> IPv4 only';
+		}
+		echo '<br><br>
 		<input type="submit" value="Next" class="formstyle">
 		</form>';
 		$this->g->Footer();
@@ -116,38 +129,50 @@ class InstallerDebian
 	
 	private function ProcessSysdata()
 	{
-		$this->g->Header();
-		if(!$this->CheckPost(array('apacheuser','iptables','arp','tempdir','wwwroot','if-internal','if-external','ip-internal','auth')))
+		if($_SESSION['update'])
 		{
-			echo 'Not all required fields were filled out.';
-			$this->g->Footer();
-			die();
-		}
-		$this->WritePostToSession(array('apacheuser','iptables','arp','tempdir','wwwroot','if-internal','if-external','ip-internal','auth'));
+			if(!$this->CheckPost(array('tempdir','wwwroot')))
+			{
+				echo 'Not all required fields were filled out.';
+				$this->g->Footer();
+				die();
+			}
+			$this->WritePostToSession(array('tempdir','wwwroot'));
+			$this->SaveMySQLStartDownload();
+		} else {
+			$this->g->Header();
+			if(!$this->CheckPost(array('apacheuser','iptables','arp','tempdir','wwwroot','if-internal','if-external','ip-internal','auth')))
+			{
+				echo 'Not all required fields were filled out.';
+				$this->g->Footer();
+				die();
+			}
+			$this->WritePostToSession(array('apacheuser','iptables','arp','tempdir','wwwroot','if-internal','if-external','ip-internal','auth'));
 		
-		echo 'Before we proceed, please make sure you have the following requirements installed:
-		<br>
-		<ul>
-		<li>sudo</li>
-		<li>iptables</li>
-		<li>wget (for downloading the latest version)</li>
-		<li>A running MySQL server (on this or another machine)</li>
-		</ul>
-		<br>
-		Please also make sure that:
-		<br>
-		<ul>
-		<li>The user running apache is allowed to write into the www-root (or is owner of that directory)</li>
-		<li>The user running apache is allowed to run iptables without password using sudo</li>
-		</ul>
-		<br>
-		You can install missing software using the following command:<br><br>
-		<code>apt-get install apache2 php5 php5-mysql sudo mysql-server</code><br><br>
-		<form action="'.$_SERVER['PHP_SELF'].'" method="get">
-		<input type="hidden" name="step" value="3">
-		<input type="submit" class="formstyle" value="Next">
-		</form>';
-		$this->g->Footer();
+			echo 'Before we proceed, please make sure you have the following requirements installed:
+			<br>
+			<ul>
+			<li>sudo</li>
+			<li>iptables</li>
+			<li>wget (for downloading the latest version)</li>
+			<li>A running MySQL server (on this or another machine)</li>
+			</ul>
+			<br>
+			Please also make sure that:
+			<br>
+			<ul>
+			<li>The user running apache is allowed to write into the www-root (or is owner of that directory)</li>
+			<li>The user running apache is allowed to run iptables without password using sudo</li>
+			</ul>
+			<br>
+			You can install missing software using the following command:<br><br>
+			<code>apt-get install apache2 php5 php5-mysql sudo mysql-server</code><br><br>
+			<form action="'.$_SERVER['PHP_SELF'].'" method="get">
+			<input type="hidden" name="step" value="3">
+			<input type="submit" class="formstyle" value="Next">
+			</form>';
+			$this->g->Footer();
+		}
 	}
 	
 	private function CheckRequirements()
@@ -197,13 +222,16 @@ class InstallerDebian
 	
 	private function SaveMySQLStartDownload()
 	{
-		if(!$this->CheckPost(array('mysql_host','mysql_user','mysql_pwd','mysql_db')))
+		if(!$_SESSION['update'])
 		{
-			echo 'Not all required fields were filled out.';
-			$this->g->Footer();
-			die();
-		}
+			if(!$this->CheckPost(array('mysql_host','mysql_user','mysql_pwd','mysql_db')))
+			{
+				echo 'Not all required fields were filled out.';
+				$this->g->Footer();
+				die();
+			}
 		$this->WritePostToSession(array('mysql_host','mysql_user','mysql_pwd','mysql_db'));
+		}
 		
 		$this->g->Header();
 		shell_exec('wget -O '.$_SESSION['tempdir'].'ov_latest.tar.gz http://sourceforge.net/projects/openvoucher/files/latest/download?source=files > /dev/null 2>&1 &');
